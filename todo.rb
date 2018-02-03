20cb38639ab021e11bf3c374b0fdab8a682db335
Ben Rodenhäuser <ben@karo.berlin>
2018-02-03 22:40:13 +0100

    list of lists, create new list

--- todo.rb
+++ todo.rb
@@ -2,6 +2,31 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
 get "/" do
-  erb "You have no lists.", layout: :layout
+  redirect "/lists"
+end
+
+get "/lists" do
+  @lists = session[:lists]
+
+  erb(:lists, layout: :layout)
+end
+
+get "/lists/new" do
+  erb(:new, layout: :layout)
+end
+
+post "/lists" do
+  name = params[:list_name]
+  session[:lists] << { name: name, todos: [] }
+  redirect "/lists"
 end
