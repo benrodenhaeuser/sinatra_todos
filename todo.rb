9a62b9d9339ca3a945578e017e2674386e9d5f78
Ben Rodenhäuser <ben@karo.berlin>
2018-02-04 09:59:06 +0100

    lesson 11: refactoring validations

--- todo.rb
+++ todo.rb
@@ -15,18 +15,32 @@ get "/" do
   redirect "/lists"
 end
 
+# GET  /lists       view lists
+# POST /lists       create new list
+# GET  /lists/new   form for creating new list
+# GET  /lists/:id   view single list
+
+# view all lists
 get "/lists" do
   @lists = session[:lists]
-
   erb(:lists, layout: :layout)
 end
 
+# render form for creating new list
 get "/lists/new" do
   erb(:new, layout: :layout)
 end
 
+# create new list
 post "/lists" do
-  name = params[:list_name]
-  session[:lists] << { name: name, todos: [] }
-  redirect "/lists"
+  name = params[:list_name].strip
+
+  if name.length > 0 &&
+    session[:lists] << { name: name, todos: [] }
+    session[:success] = "List has been succesfully created!"
+    redirect "/lists"
+  else
+    session[:error] = "List names have to contain at least one character."
+    erb(:new, layout: :layout)
+  end
 end
