b2e2727007b5a151db944611c91b8351d15e58fe
Ben Rodenhäuser <ben@karo.berlin>
2018-02-05 07:45:21 +0100

    single list view and adding todos

--- todo.rb
+++ todo.rb
@@ -9,38 +9,67 @@ end
 
 before do
   session[:lists] ||= []
+  @lists = session[:lists]
 end
 
 get "/" do
   redirect "/lists"
 end
 
-# GET  /lists       view lists
-# POST /lists       create new list
-# GET  /lists/new   form for creating new list
-# GET  /lists/:id   view single list
-
 # view all lists
 get "/lists" do
-  @lists = session[:lists]
   erb(:lists, layout: :layout)
 end
 
-# render form for creating new list
+# render new list form
 get "/lists/new" do
-  erb(:new, layout: :layout)
+  erb(:new_list, layout: :layout)
+end
+
+# render new todo form
+get "/lists/:id/new" do
+  @id = params[:id].to_i
+  erb(:new_todo, layout: :layout)
+end
+
+# view single list
+get "/lists/:id" do
+  @id = params[:id].to_i
+  if @id < @lists.size
+    @list = @lists[@id]
+    erb(:list, layout: :layout)
+  else
+    session[:error] = "The requested list does not exist."
+    redirect "/lists"
+  end 
 end
 
 # create new list
 post "/lists" do
-  name = params[:list_name].strip
+  list_name = params[:list_name].strip
 
-  if name.length > 0 &&
-    session[:lists] << { name: name, todos: [] }
+  if list_name.length > 0
+    @lists << { name: list_name, todos: [] }
     session[:success] = "List has been succesfully created!"
     redirect "/lists"
   else
     session[:error] = "List names have to contain at least one character."
-    erb(:new, layout: :layout)
+    erb(:new_list, layout: :layout)
+  end
+end
+
+# create new todo
+post "/lists/:id" do
+  @id = params[:id].to_i
+  @list = @lists[@id]
+  todo = params[:todo].strip
+
+  if todo.length > 0
+    @list[:todos] << todo
+    session[:success] = "Todo has been succesfully added!"
+    redirect "/lists/#{@id}"
+  else
+    session[:error] = "Todos have to contain at least one character."
+    erb(:new_todo, layout: :layout)
   end
 end
